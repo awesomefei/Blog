29cd44e2a8da341205d99f94cbb8045cf45c8ff3
Ann <lifeiwanguk@gmail.com>
2018-06-03 22:51:55 -0700

    adding users in admin

--- CMS/admin/includes/add_user.php
+++ CMS/admin/includes/add_user.php
@@ -34,37 +34,34 @@ if(isset($_POST['create_post'])) {
    }    
 ?> 
 <form action="" method="post" enctype="multipart/form-data">    
- <div class="form-group">
-     <label for="title">Post Title</label>
-      <input type="text" class="form-control" name="post_title">
+
+<div class="form-group">
+     <label for="title">Firstname</label>
+      <input type="text" class="form-control" name="user_firstname">
 </div>
 
 <div class="form-group">
-     <label for="title">Post Author</label>
-      <input type="text" class="form-control" name="post_author">
+     <label for="title">Lastname</label>
+      <input type="text" class="form-control" name="user_lastname">
 </div>
 
+<!--
 <div class="form-group">
-    <label for="title">Post Category</label>
+    <label for="title">Add User</label>
      <select name="post_category" id="post_cat">
 <?php
-    $query = "SELECT * FROM category";
-    $select_cat = mysqli_query($connection,$query);
-    confirmQuery($select_cat);
-    while($row = mysqli_fetch_assoc($select_cat)){
-        $cat_title = $row['cat_title'];
-        $cat_id = $row['cat_id']; 
-        echo "<option value='{$cat_id}'>{$cat_title}</option>";
-    }
+//    $query = "SELECT * FROM users";
+//    $select_cat = mysqli_query($connection,$query);
+//    confirmQuery($select_cat);
+//    while($row = mysqli_fetch_assoc($select_cat)){
+//        $cat_title = $row['cat_title'];
+//        $cat_id = $row['cat_id']; 
+//        echo "<option value='{$cat_id}'>{$cat_title}</option>";
+//    }
 ?>
     </select>
 </div>
-
-
-<div class="form-group">
-     <label for="title">Post Status</label>
-      <input type="text" class="form-control" name="post_status">
-</div>
+-->
 
 <div class="form-group">
      <select name="post_status" id="">
@@ -74,26 +71,30 @@ if(isset($_POST['create_post'])) {
      </select>
   </div>
       
+<!--
     <div class="form-group">
          <label for="post_image">Post Image</label>
           <input type="file"  name="image">
-      </div>
+    </div>
+-->
 
       <div class="form-group">
-         <label for="post_tags">Post Tags</label>
-          <input type="text" class="form-control" name="post_tags">
+         <label for="post_tags">Username</label>
+          <input type="text" class="form-control" name="username">
       </div>
       
       <div class="form-group">
-         <label for="post_content">Post Content</label>
-         <textarea class="form-control "name="post_content" id="" cols="30" rows="10">
-         </textarea>
+         <label for="user_email">Email</label>
+         <input type="email" class="form-control" name="user_email">
       </div>
       
+      <div class="form-group">
+         <label for="user_password">Password</label>
+         <input type="password" class="form-control" name="user_password">
+      </div>
       
-
        <div class="form-group">
-          <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+          <input class="btn btn-primary" type="submit" name="create_user" value="Add User">
       </div>
 
 
